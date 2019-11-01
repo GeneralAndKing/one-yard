03f9ac60dc3118ef327f860451a5dfd83285fb35
EchoCow <lizhongyue248@163.com>
2019-11-01 08:23:02 +0800

    :tada: Initial commit.

---
+++ src/main/java/in/gaks/oneyard/OneYardApplication.java
@@ -0,0 +1,13 @@
+package in.gaks.oneyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneYardApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OneYardApplication.class, args);
+  }
+
+}
---
+++ src/test/java/in/gaks/oneyard/OneYardApplicationTests.java
@@ -0,0 +1,13 @@
+package in.gaks.oneyard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneYardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
