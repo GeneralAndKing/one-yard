649a9425ebd07aef4b28c5030e155498595d843c
EchoCow <lizhongyue248@163.com>
2019-11-03 17:13:39 +0800

    :construction: one-yard-generate work in progress.!

---
+++ one-yard-generate/src/main/java/in/gaks/oneyard/Application.java
@@ -0,0 +1,19 @@
+package in.gaks.oneyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类.
+ *
+ * @author <a href="https://echocow.cn">EchoCow</a>
+ * @date 2019/11/3 下午5:04
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
