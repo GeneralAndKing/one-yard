a679fcdd6b2a17df3a7d52f3eedc0451efe0eee9
EchoCow <lizhongyue248@163.com>
2019-11-06 22:53:16 +0800

    :wheelchair: Improving accessibility.

---
+++ src/main/java/in/gaks/oneyard/handle/ResourceExceptionHandle.java
@@ -0,0 +1,63 @@
+package in.gaks.oneyard.handle;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import in.gaks.oneyard.model.exception.ResourceException;
+import in.gaks.oneyard.model.exception.ResourceNotFoundException;
+import in.gaks.oneyard.model.exception.ValidationException;
+import javax.validation.ConstraintViolationException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 资源异常捕获处理.
+ *
+ * @author <a href="https://echocow.cn">EchoCow</a>
+ * @date 2019/11/6 下午10:40
+ */
+@RestControllerAdvice
+public class ResourceExceptionHandle {
+
+  /**
+   * ConstraintViolationException.
+   *
+   * @param exception exception
+   * @return 400
+   */
+  @ExceptionHandler(ConstraintViolationException.class)
+  public HttpEntity<?> handle(ConstraintViolationException exception) {
+    JSONArray errors = new JSONArray();
+    exception.getConstraintViolations().forEach(item -> errors.add(item.getMessage()));
+    return ResponseEntity.badRequest().body(errors);
+  }
+
+  /**
+   * ResourceNotFoundException.
+   *
+   * @param exception exception
+   * @return 404
+   */
+  @ExceptionHandler(ResourceNotFoundException.class)
+  public HttpEntity<?> handleResourceNotFoundException(ResourceNotFoundException exception) {
+    JSONObject error = new JSONObject();
+    error.put("error_message", exception.getMessage());
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+  }
+
+  /**
+   * ResourceException.
+   *
+   * @param exception exception
+   * @return 400
+   */
+  @ExceptionHandler({ResourceException.class, ValidationException.class})
+  public HttpEntity<?> handleResourceException(Exception exception) {
+    JSONObject error = new JSONObject();
+    error.put("error_message", exception.getMessage());
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+  }
+
+}
