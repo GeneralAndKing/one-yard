34519aea4632b4f0684af3b26a85796bc2fd6d82
LiuChuanJin <1157464268@qq.com>
2019-11-12 20:43:23 +0800

    :sparkles: update notification

--- src/main/java/in/gaks/oneyard/service/impl/MaterialPlanServiceImpl.java
+++ src/main/java/in/gaks/oneyard/service/impl/MaterialPlanServiceImpl.java
@@ -104,17 +104,18 @@ public class MaterialPlanServiceImpl extends BaseServiceImpl<MaterialDemandPlanR
     Notification notification = new Notification();
     if ("审批通过".equals(approval.getResult())) {
       notification.setName("需求计划审批通过通知");
-      notification.setContent("您于" + materialDemandPlan.getCreateTime()
+      notification.setMessage("您于" + materialDemandPlan.getCreateTime()
           + "提报创建的 " + materialDemandPlan.getName() + " 审批通过了！");
     } else {
       notification.setName("需求计划审批退回通知");
-      notification.setContent("您于" + materialDemandPlan.getCreateTime()
+      notification.setMessage("您于" + materialDemandPlan.getCreateTime()
           + "提报创建的 " + materialDemandPlan.getName() + " 因为某些原因被退回了。");
     }
-    notificationRepository.save(notification);
     // 获取通知接收方id
     SysUser user = sysUserRepository.findFirstByUsername(materialDemandPlan.getCreateUser())
         .orElseThrow(() -> new ResourceNotFoundException("该计划的提报员查询失败"));
+    notification.setReceiverId(user.getId());
+    notificationRepository.save(notification);
     // 检测用户是否在线发送通知
     notifyUtil.sendMessage(user.getId().toString(), notification);
   }
