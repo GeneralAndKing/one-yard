1f9e1887838e752203cd8edd8739d2a3fce92be1
LiuChuanJin <1157464268@qq.com>
2019-11-12 23:50:42 +0800

    :sparkles: update

--- src/main/java/in/gaks/oneyard/repository/ApprovalRepository.java
+++ src/main/java/in/gaks/oneyard/repository/ApprovalRepository.java
@@ -1,9 +1,13 @@
 package in.gaks.oneyard.repository;
 
 import in.gaks.oneyard.base.BaseRepository;
+import in.gaks.oneyard.model.constant.ApprovalTypeStatus;
 import in.gaks.oneyard.model.constant.OneYard;
 import in.gaks.oneyard.model.entity.Approval;
+import java.util.List;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Repository.
@@ -13,4 +17,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(path = OneYard.AUTH)
 public interface ApprovalRepository extends BaseRepository<Approval, Long> {
 
+  /**
+   * 根据计划id和审批类型查询审批流程.
+   *
+   * @param planId 计划id
+   * @param approvalType 审批类型
+   * @return .
+   */
+  @RestResource(path = "byPlanIdAndApprovalType")
+  List<Approval> findByPlanIdAndApprovalType(@Param("planId") Long planId,
+      @Param("approvalType") ApprovalTypeStatus approvalType);
 }
