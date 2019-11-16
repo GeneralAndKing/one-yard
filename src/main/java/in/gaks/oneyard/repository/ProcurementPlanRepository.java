c618c2a317e1926452e8c31ec7c57016eb39f7d6
LiuChuanJin <1157464268@qq.com>
2019-11-17 03:13:25 +0800

    :sparkles: 采购计划表rest接口

--- src/main/java/in/gaks/oneyard/repository/ProcurementPlanRepository.java
+++ src/main/java/in/gaks/oneyard/repository/ProcurementPlanRepository.java
@@ -1,9 +1,14 @@
 package in.gaks.oneyard.repository;
 
 import in.gaks.oneyard.base.BaseRepository;
+import in.gaks.oneyard.model.constant.ApprovalStatus;
 import in.gaks.oneyard.model.constant.OneYard;
+import in.gaks.oneyard.model.constant.PlanStatus;
 import in.gaks.oneyard.model.entity.ProcurementPlan;
+import java.util.List;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 /**
  * Repository.
@@ -14,4 +19,24 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource(path = OneYard.PROCUREMENT_PLAN)
 public interface ProcurementPlanRepository extends BaseRepository<ProcurementPlan, Long> {
 
+  /**
+   * 根据状态查询采购计划.
+   *
+   * @param planStatus 计划id
+   * @param approvalStatus 审批id
+   * @return 。
+   */
+  @RestResource(path = "byStatus")
+  List<ProcurementPlan> findAllByPlanStatusAndApprovalStatus(
+      @Param("planStatus") PlanStatus planStatus,
+      @Param("approvalStatus") ApprovalStatus approvalStatus);
+
+  /**
+   * 根据创建者查询采购计划.
+   *
+   * @param createUser 创建者的username
+   * @return .
+   */
+  @RestResource(path = "byCreateUser")
+  List<ProcurementPlan> findAllByCreateUser(@Param("createUser") String createUser);
 }
