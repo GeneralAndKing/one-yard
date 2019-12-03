b4858df0e6c2e95d26f77064e6d7470ddaa9d282
EchoCow <lizhongyue248@163.com>
2019-12-03 11:00:42 +0800

    :sparkling_heart: Add delete log methods!

--- src/main/java/in/gaks/oneyard/repository/SysLogRepository.java
+++ src/main/java/in/gaks/oneyard/repository/SysLogRepository.java
@@ -3,7 +3,12 @@ package in.gaks.oneyard.repository;
 import in.gaks.oneyard.base.BaseRepository;
 import in.gaks.oneyard.model.constant.OneYard;
 import in.gaks.oneyard.model.entity.SysLog;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Repository.
@@ -13,5 +18,17 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
  */
 @RepositoryRestResource(path = OneYard.SYS_LOG)
 public interface SysLogRepository extends BaseRepository<SysLog, Long> {
+
+  /**
+   * 删除.
+   *
+   * @param createTime 创建时间
+   * @return 操作条数
+   */
+  @Modifying
+  @RestResource(path = "deleteByCreateTime")
+  @Transactional(rollbackForClassName = "java.lang.Exception")
+  @Query(value = "update sys_log set is_enable = false where create_time <= :createTime", nativeQuery = true)
+  int deleteByCreateTime(@Param("createTime") String createTime);
 
 }
